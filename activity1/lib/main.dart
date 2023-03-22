c7f18d392c15bec223c70ea1b8871a8e1109e98f
IngAndresRincon <andres05_31@hotmail.com>
2023-03-22 18:45:20 -0500

    Se cambia el nombre al archivo

--- activity1/lib/main.dart
+++ activity1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:activity1/widgets/hola_mundo.dart';
+import 'package:activity1/widgets/imagen_circular.dart';
 
 void main() {
   runApp(const ImageView());

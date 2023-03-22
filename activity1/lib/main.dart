f43f008014939ba956e3b0069ec1d9bd05322326
IngAndresRincon <andres05_31@hotmail.com>
2023-03-22 18:42:40 -0500

    Se crea imagen circular

--- activity1/lib/main.dart
+++ activity1/lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:activity1/widgets/hola_mundo.dart';
+
 void main() {
-  runApp(const Screen());
-}
-
-class Screen extends StatelessWidget {
-  const Screen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      //Inicializa Material Design
-      title: "Clases Flutter",
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Prueba App"),
-        ),
-        body: ,
-      ),
-    );
-  }
+  runApp(const ImageView());
 }
--- activity1/lib/widgets/hola_mundo.dart
+++ activity1/lib/widgets/hola_mundo.dart
@@ -1,3 +1,37 @@
 import 'package:flutter/material.dart';
 
+class ImageView extends StatelessWidget {
+  const ImageView({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Padding SafeArea',
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.blueGrey,
+                  width: 3,
+                ),
+              ),
+              child: ClipOval(
+                child: Image.asset(
+                  "assets/imagen1.jpg",
+                  fit: BoxFit.fitHeight,
+                  width: 80,
+                  height: 80,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
